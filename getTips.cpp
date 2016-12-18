42b41d78f0a217d726e351856e7b202a259ccfa3
BeschtaBrandon <blbeschta@wisc.edu>
2016-12-17 19:55:04 -0600

    gitignore and initmap function

--- getTips.cpp
+++ getTips.cpp
@@ -7,32 +7,33 @@
 using namespace std;
 
 string getHealthTip(unordered_map<int, string> healthTips, int index) {
-	string result = healthTips[index];
-	return result;
+	return healthTips[index];
 }
 
-int main () {
-
-	unordered_map<int, string> healthTips;	
+void initMap(unordered_map<int, string> &map) {
 	int count = 0;
-  
+
 	ifstream inFile("tips");
 	if (!inFile.is_open()) {
-		return -1;
+		exit(-1);
 	}
 
 	while(true) {
-        string sName;
-        // The line below doesn't work if the name has space(s).
-        // inFile >> sName;
-        // Read a line from the input file and save it in string.
-        getline(inFile, sName);
-				if (inFile.fail()) {	
-			    break;
-      	}
-				healthTips.insert({count++, sName});
+    string line;
+    getline(inFile, line);
+		if (inFile.fail()) {
+	    break;
+  	}
+		map.insert({count++, line});
 	}
+}
+
+int main () {
+	unordered_map<int, string> healthTips;
+	initMap(healthTips);
 
 	string result = getHealthTip(healthTips, 2);
 	cout << result << endl;
+
+	return 1;
 }
